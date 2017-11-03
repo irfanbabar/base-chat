050c7017f1179526a631243bfad355e511333758
Hammad Maqbool <hammad417@gmail.com>
2017-11-03 20:06:23 +0500

    added new chagnes

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+import { Observable } from 'rxjs/Observable';
+
+import { ChatService }  from "../services/chat.service";
+import { ChatMessage } from '../models/chat-message.model';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent implements OnInit {
-
-  constructor() { }
+export class FeedComponent implements OnInit, OnChanges {
+  feed: Observable<ChatMessage[]>;
+  constructor(private db: AngularFireDatabase, private chatService: ChatService) { }
 
   ngOnInit() {
+    this.feed = this.db.list('messages', ref=> ref.limitToLast(25).orderByKey(true)).valueChanges();
   }
 
+  ngOnChanges(){
+    // this.feed = this.chatService.getMessages();
+    this.feed = this.db.list('messages', ref=> ref.limitToLast(25)).valueChanges();
+  }
 }
--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { ChatMessage } from "../models/chat-message.model";
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
+  @Input() chatMessage: ChatMessage;
+  userEmail: string;
+  userName: string;
+  timeStamp: string;
+  messageContent: string;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(chat = this.chatMessage) {
+    this.userEmail = chat.email;
+    this.userName = chat.userName;
+    this.messageContent = chat.message;
+    this.timeStamp = chat.timeSent;
   }
 
 }
